37f95c4bb9271bbd6fed858ea680098851b49674
Judith lobo <6516127-Judithlobo@users.noreply.replit.com>
2024-11-05 21:10:24 +0000

    refactor

---
+++ session2.py
@@ -0,0 +1,15 @@
+# ===  EXPRESSION
+# 1+1 or "hello"
+# expression return a value
+
+
+# === STATEMENT
+# statemnt is a line of code that performs an action 
+# statement is an operation on a value 
+# data = 1+1
+# print(data)
+
+
+# Note
+# we can have multiple statements in a single line using a semicolon
+data=1+1 ; print(data)
